7310c6bebf5ab115ef5d4db18b380051f1991139
Dade1991 <dade91@msn.com>
2025-07-10 10:01:00 +0200

    modded

--- (Stefano)FS0325-U2-W3-D3-main/meteo-app/meteo-app.js
+++ (Stefano)FS0325-U2-W3-D3-main/meteo-app/meteo-app.js
@@ -2,16 +2,16 @@
 
 const getBobbioWeather = function () {
   fetch(
-    'https://api.open-meteo.com/v1/forecast?latitude=44.7683&longitude=9.3841&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&current=temperature_2m,weather_code&timezone=Europe%2FBerlin&forecast_days=1'
+    "https://api.open-meteo.com/v1/forecast?latitude=44.7683&longitude=9.3841&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&current=temperature_2m,weather_code&timezone=Europe%2FBerlin&forecast_days=1"
   )
-    .then((res) => {
-      console.log('RESPONSE', res)
-      if (res.ok) {
+    .then((response) => {
+      console.log("RESPONSE", response)
+      if (rseponse.ok) {
         // la risposta dal server è ok, estrapoliamo il JSON
-        return res.json()
+        return response.json()
       } else {
         // la Response ha un problema!
-        throw new Error('La response da Open Meteo non è ok')
+        throw new Error("La response da Open Meteo non è ok")
       }
     })
     .then((meteoObj) => {
@@ -19,17 +19,17 @@ const getBobbioWeather = function () {
       console.log(meteoObj.daily.temperature_2m_min[0])
       console.log(meteoObj.daily.temperature_2m_max[0])
       //   è solo qui dentro che posso utilizzare meteoObj!
-      const minSpan = document.getElementById('min-temp')
-      const maxSpan = document.getElementById('max-temp')
+      const minSpan = document.getElementById("min-temp")
+      const maxSpan = document.getElementById("max-temp")
       minSpan.innerText = meteoObj.daily.temperature_2m_min[0]
       maxSpan.innerText = meteoObj.daily.temperature_2m_max[0]
 
       //   metto anche la temp corrente
-      const currentTempSpan = document.getElementById('current-temp')
+      const currentTempSpan = document.getElementById("current-temp")
       currentTempSpan.innerText = meteoObj.current.temperature_2m
     })
     .catch((err) => {
-      console.log('ERRORE!', err)
+      console.log("ERRORE!", err)
     })
 }
 
